e457ab7a774a2adbd9baf5d877bb554063eae887
Szalkai Krisztián Farkas <szalkaititi@gmail.com>
2020-11-26 14:02:55 +0100

    added swagger

--- MuNyi.Web/Startup.cs
+++ MuNyi.Web/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MuNyi.Bll.ServiceInterfaces;
 using MuNyi.Bll.Services;
 using MuNyi.Dal;
@@ -52,12 +53,11 @@ namespace MuNyi.Web
                 options.User.RequireUniqueEmail = true;
             });
 
-            services.AddIdentity<User, IdentityRole>()
-               .AddEntityFrameworkStores<MuNyiContext>()
-               .AddDefaultTokenProviders();
-
             services.AddAuthentication();
             services.AddAuthorization();
+            services.AddIdentity<User, IdentityRole>()
+               .AddEntityFrameworkStores<MuNyiContext>()
+               .AddDefaultTokenProviders();            
 
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
             services.AddAuthentication(options =>
@@ -77,7 +77,40 @@ namespace MuNyi.Web
                     ValidAudience = Configuration["Jwt:Issuer"],
                     ClockSkew = TimeSpan.Zero // remove delay of token when expire
                 };
-            });            
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MuNyi API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+            });
 
             services.AddControllers();
 
@@ -116,7 +149,11 @@ namespace MuNyi.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MuNyi API V1");
+            });
             app.UseRouting();
 
             app.UseAuthentication();
